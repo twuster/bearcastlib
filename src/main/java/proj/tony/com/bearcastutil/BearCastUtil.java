2232ba8eb26e1cb56759420143cdeed5a41608cd
Tony Wu <tony.wu@berkeley.edu>
2015-04-30 01:02:33 -0700

    initial push

---
+++ src/main/java/proj/tony/com/bearcastutil/BearCastUtil.java
@@ -0,0 +1,328 @@
+package proj.tony.com.bearcastutil;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import edu.berkeley.sdb.blescanner.BLEScanner;
+
+/**
+ * Created by tonywu on 4/29/15.
+ */
+public class BearCastUtil {
+
+    private static final String TAG = BearCastUtil.class.getSimpleName();
+    private static final long TIMER_PERIOD = 5000;
+    private static final long TIMER_DELAY = 500;
+    private static final String FIRESTORM_MAC = "EC:C1:8E:1D:48:60";
+    private static final String TEST_LOCATION = "USA/California/Berkeley/Soda/410";
+    private static final String FIRESTORM_MAC2 = "F9:86:10:B1:19:F9";
+    private static final String TEST_LOCATION2 = "USA/California/Berkeley/Soda/510";
+    private static final String TEST_IP = "136.152.38.117";
+    private static final String SERVER_PORT = "8080";
+    private static final String MQTT_SERVER = "tcp://54.215.11.207:9009";
+    private static final String DISCOVERY_TOPIC = "bearcast_registration";
+
+    // Key for user shared prefenreces
+    private static final String USER_PREFS_NAME = "UserKey";
+    // Shared Preferences
+    SharedPreferences mUserPreferences;
+
+    private MqttAndroidClient mMQTTClient;
+    private MqttCallback mMqttCallback;
+    private String mResultTopic;
+    private String muuid;
+    private HashMap<Long, String> mRequestMapping;
+
+    private static String sClosestDisplayTopic;
+    private static String sCastText;
+    private static Context sContext;
+
+    /*
+    * Gets the number of available cores
+    * (not always the same as the maximum number of cores)
+    */
+    private static int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
+    // Sets the amount of time an idle thread waits before terminating
+    private static final int KEEP_ALIVE_TIME = 1;
+    // Sets the Time Unit to seconds
+    private static final TimeUnit KEEP_ALIVE_TIME_UNIT = TimeUnit.SECONDS;
+    // Creates a thread pool manager
+    private ExecutorService mNetworkThreadPool = new ThreadPoolExecutor(
+            NUMBER_OF_CORES,       // Initial pool size
+            NUMBER_OF_CORES,       // Max pool size
+            KEEP_ALIVE_TIME,
+            KEEP_ALIVE_TIME_UNIT,
+            new LinkedBlockingQueue<Runnable>());
+
+    private String mCurrentLocation;
+    private String mCurrentClosestMac;
+
+    private HashMap<String,String> mBleMacToLocation;
+
+    private Timer mBleTimer;
+    private BLEScanner mBleScanner;
+    private BleCallback mBleCallback;
+
+
+    public BearCastUtil(Context context) {
+        sContext = context;
+
+        mBleScanner = new BLEScanner(sContext);
+        if (!mBleScanner.enable()) {
+            Log.e(TAG, "Failed to initialize BLE scanner");
+        }
+
+        mBleMacToLocation = new HashMap<String,String>(); // TODO: Hard code this map for now
+        mBleMacToLocation.put(FIRESTORM_MAC, TEST_LOCATION);
+        mBleMacToLocation.put(FIRESTORM_MAC2, TEST_LOCATION2);
+
+        muuid = DeviceUUID.getDeviceUUID(sContext).toString();
+        sClosestDisplayTopic = null;
+        mRequestMapping = new HashMap<Long, String>();
+        mMqttCallback = new MyMqttCallback();
+
+        mMQTTClient = new MqttAndroidClient(sContext, MQTT_SERVER, MqttClient.generateClientId());
+        mMQTTClient.setCallback(mMqttCallback);
+        mNetworkThreadPool.execute(new ConnectRunnable());
+
+        mBleTimer = new Timer();
+        mBleCallback = new BleCallback();
+    }
+
+
+    public void startBluetoothScan() {
+        mBleTimer.scheduleAtFixedRate(new BleScannerTask(), TIMER_DELAY, TIMER_PERIOD);
+    }
+
+    public void stopBlueToothScan() {
+        mBleTimer.cancel();
+    }
+
+
+    public void castMessage(String text) {
+        sCastText = text;
+        if (sClosestDisplayTopic != null) {
+            mNetworkThreadPool.execute(new CastRunnable());
+            Toast.makeText(sContext, "Sending Message", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(sContext, "Waiting to Discover Closest Device", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Gets the client's current location
+    // TODO: Link this with BearLoc to get real location
+    private String getLocation() {
+        if (mCurrentClosestMac != null){
+            mCurrentLocation = mBleMacToLocation.get(mCurrentClosestMac);
+        }
+        return mCurrentLocation;
+    }
+
+    private class MyMqttCallback implements MqttCallback {
+        @Override
+        public void connectionLost(Throwable cause) {
+            Log.d(TAG, "MQTT Server connection lost");
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage message) {
+            Log.d(TAG, String.format("Message arrived: %s: %s", topic, message.toString()));
+
+            if (!topic.equals(mResultTopic)) {
+                return;
+            }
+
+            JSONObject msg = null;
+
+            try {
+                String payload = new String(message.getPayload());
+                msg = new JSONObject(payload);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                long reqNum = msg.getLong("req_num");
+                if (mRequestMapping.containsKey(reqNum)) {
+                    if (mRequestMapping.get(reqNum) == "discover") {
+                        JSONObject result = msg.getJSONObject("result");
+                        sClosestDisplayTopic = result.getString("server_topic");
+                        Log.d(TAG, "Received topic: " + sClosestDisplayTopic);
+                        mRequestMapping.remove(reqNum);
+                    } else if (mRequestMapping.get(reqNum) == "cast") {
+                        Log.d(TAG, "FINISHED CASTING");
+                        mRequestMapping.remove(reqNum);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+            Log.d(TAG, "Delivery complete");
+        }
+    }
+
+
+    private class CastRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            Log.d(TAG, "CASTING");
+
+            final JSONObject json = new JSONObject();
+            Long epoch = System.currentTimeMillis()/1000;
+
+            try {
+                json.put("req_type", "cast");
+                final JSONObject reqContent = new JSONObject();
+
+                String message = sCastText;
+                reqContent.put("data", message);
+                json.put("req_content", reqContent);
+
+                mResultTopic = muuid + "-reply";
+                json.put("reply_topic", muuid + "-reply");
+
+                json.put("req_num", epoch);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                MqttMessage message = new MqttMessage();
+                message.setPayload(json.toString().getBytes());
+                mRequestMapping.put(epoch, "cast");
+                IMqttDeliveryToken token = mMQTTClient.publish(sClosestDisplayTopic, message);
+                token.waitForCompletion();
+                Log.d(TAG, "DONE CASTING");
+
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+
+
+    private class ConnectRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+            try {
+                IMqttToken token = mMQTTClient.connect(options);
+                token.waitForCompletion();
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class DiscoverRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            Log.d(TAG, "RUNNING");
+            final JSONObject json = new JSONObject();
+            Long epoch = System.currentTimeMillis()/1000;
+
+            try {
+                mResultTopic = muuid + "-reply";
+                IMqttToken token = mMQTTClient.subscribe(mResultTopic, 0);
+                token.waitForCompletion();
+            } catch (MqttException e ) {
+                e.printStackTrace();
+            }
+
+            try {
+                json.put("req_type", "discover");
+                final JSONObject reqContent = new JSONObject();
+                reqContent.put("location", getLocation());
+                json.put("req_content", reqContent);
+
+                json.put("reply_topic", mResultTopic);
+
+                json.put("req_num", epoch);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Log.d(TAG, "POST MESSAGE");
+
+                MqttMessage message = new MqttMessage();
+                message.setPayload(json.toString().getBytes());
+                mRequestMapping.put(epoch, "discover");
+                IMqttDeliveryToken token = mMQTTClient.publish(DISCOVERY_TOPIC, message);
+                token.waitForCompletion();
+                Log.d(TAG, "DONE");
+
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private class BleScannerTask extends TimerTask {
+        @Override
+        public void run() {
+            mBleScanner.getBLEDeviceMap(mBleCallback);
+        }
+    }
+
+    // Callback for BLE Scanner
+    private class BleCallback implements BLEScanner.BluetoothScannerCallBack {
+        @Override
+        public void onMapReturned(HashMap<String, Integer> signalStrengths) {
+            String newClosestMac = null;
+            int maxStrength = Integer.MIN_VALUE;
+
+            for (HashMap.Entry<String,Integer> e : signalStrengths.entrySet()) {
+                String macAddr = e.getKey();
+                int signalStrength = e.getValue();
+                Log.d(TAG, "RSSI: " + signalStrength);
+
+                if (signalStrength > maxStrength && mBleMacToLocation.containsKey(macAddr)) {
+                    Log.d(TAG, "Found closest mac address: " + macAddr);
+                    maxStrength = signalStrength;
+                    newClosestMac = macAddr;
+                }
+            }
+
+            if (newClosestMac != null && !newClosestMac.equals(mCurrentClosestMac)) {
+                mCurrentClosestMac = newClosestMac;
+            }
+            mNetworkThreadPool.execute(new DiscoverRunnable());
+        }
+    }
+}
